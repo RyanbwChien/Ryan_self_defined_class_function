bb90c1dfa5fdce51cf79567decab5bb0888b7232
MSI\USER <saikidd2002@yahoo.com.tw>
2026-07-02 18:41:10 +0800

    no message

--- [Practice] Ryan_DAG_matrix_from_scratch_without_Indegree_Tensorflow_stytle_simplify_from_scratch.py
+++ [Practice] Ryan_DAG_matrix_from_scratch_without_Indegree_Tensorflow_stytle_simplify_from_scratch.py
@@ -1,74 +1,84 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu May  7 11:52:08 2026
+import torch
+import torch.nn as nn
 
-@author: USER
-"""
+d_model = 64
+nhead = 8
+num_layers = 4
+
+encoder_layer = nn.TransformerEncoderLayer(d_model, nhead)
+nn.TransformerEncoder(encoder_layer, num_layers)
+
+import tensorflow as tf
+# tf.GradientTapegradient(target, sources)
+
+
 import numpy as np
 
-GRAD_MAP = {}
-# 模組自動註冊（超重要）
-def register_grad(ops):
-    def decorator(func):
-        GRAD_MAP[ops] = func
+Grad_func_map = {}
+
+
+def apply_method(op_name,inputs):
+    
+    if op_name == "MatMul":
+        res =  inputs[0].data @ inputs[1].data
+    
+    ouput = Tensor(res)
+    
+    if _Current_Tape is not None:
+        _Current_Tape.record(op_name, inputs, ouput)
+
+
+def _register_method(op_name):
+    def decorator(func):        
+        Grad_func_map[op_name] = func
         return func
+        
     return decorator
-# 目前這寫法不會改變原來函數 只是會去多執行GRAD_MAP[ops] = func
-# func = func
 
-@register_grad("Add")
-def Add_grad(grad, inputs):   
-    return grad , grad
 
-@register_grad("Mul")
-def Mul_grad(grad, inputs):   
-    return grad*inputs[1] , grad*inputs[0]
 
-class Variable:
-    def __init__(self,value):
-        self.value = np.array(value)
-        self.id = id(self)
-    def __add__(self,other):
-        return apply_op("Add",self,other)
-    
-    
-def apply_op(ops, left, right):
-    if ops == "Add": 
-        out = left.value + right.value
-        
-    out = Variable(out)
-    if _CURRENT_TAPE:
-        _CURRENT_TAPE.record_ops([ops,[left, right], out])
-        
+def mat_mul(inputs):
+    out = inputs[0].data @ inputs[1].data
     return out
 
 
 
 
 
-     
-class GradientTape:
+
+class Tensor:
+    def __init__(self,x):
+        self.data = np.array(x,dtype=np.int32)
+        self.id = id(self)
+    
+    def __matmul__(self,inputs):
+        apply_method
+        return apply_method("MatMul",inputs)
+
+    
+
+    
+class Variable(Tensor):
+    def __init__(self):
+        super().__init__()
+        
+_Current_Tape = None
+
+class RyGrandientTape:
     def __enter__(self):
-        global _CURRENT_TAPE
-        _CURRENT_TAPE = self
-        self.history = []
+        global _Current_Tape
+        if _Current_Tape is None:
+            self.history = []
+            _Current_Tape = self
         return self
+            
     def __exit__(self):
-        global _CURRENT_TAPE
-        _CURRENT_TAPE = None
-    def record_ops(self, op_name, inputs, output):
-        self.history.append([op_name, inputs, output])
-    def gradient(self, target, weight):
-        grads = {target.id:np.ones_like(target.values)}
+        global _Current_Tape
+        _Current_Tape = None
         
-        for op_name, inputs, output in reversed(self.history):
-            GRAD_MAP[op_name]
+    def record(self,op_name,inputs,ouput):
+        self.history.append([op_name,inputs,ouput])
         
         
-        
-        
-        
-        
-        
-        
-        
+    def gradient(self, target, source):
+        pass
